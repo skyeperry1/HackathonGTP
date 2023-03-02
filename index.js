c6d7aa0c5f9907e540edf5a7466ac39e629b1cdf
Perry <perrs@l36774mus.lan>
2023-03-02 18:31:47 -0500

    refactor customer handler

--- index.js
+++ index.js
@@ -145,24 +145,25 @@ function handle_customer(message) {
                 }
             );
         }
+        if (message.type == "text") {
+            if (message.text.includes("Thank you. What billing question can we help you with")) {
+                DMS.sendTextMessage(
+                    customer.id, //
+                    customer.last_msg_id++, //Unique id of the message
+                    "I need to change my address",
+                    customer.bio.name,
+                    function (response) {
+                        //Return status from DMS
+                        //return res.status(response.status).send(response.statusText);
+                        customers[message.customer_id].last_msg_id++;
+                        customers[message.customer_id].state = "escalating";
+                    }
+                );
+            }
 
-        if (message.type == "text" && message.text.includes("Thank you. What billing question can we help you with")) {
-            DMS.sendTextMessage(
-                customer.id, //
-                customer.last_msg_id++, //Unique id of the message
-                "I need to change my address",
-                customer.bio.name,
-                function (response) {
-                    //Return status from DMS
-                    //return res.status(response.status).send(response.statusText);
-                    customers[message.customer_id].last_msg_id++;
-                    customers[message.customer_id].state = "escalating";
-                }
-            );
-        }
-
-        if (message.type == "text" && message.text.includes("connected")) {
-            customers[message.customer_id].state = "connected";
+            if (message.text.includes("connected")) {
+                customers[message.customer_id].state = "connected";
+            }
         }
     } catch (err) {
         console.log(err);
