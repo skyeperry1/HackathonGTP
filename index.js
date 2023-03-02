9ec472572270e098c8ec2063ee60b275da94fe5c
Perry <perrs@l36774mus.lan>
2023-03-02 18:13:26 -0500

    refactor customer handler

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ function handle_customer(message) {
     }
 
 
-    if (message.title.trim === "What do you need help with?") {
+    if (message.title.trim() === "What do you need help with?") {
         DMS.sendTextMessage(
             customer.id, //
             customer.last_msg_id++, //Unique id of the message
