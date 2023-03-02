f96263c7c06182b3b3aac1cfed1b6b925a18cd37
Perry <perrs@l36774mus.lan>
2023-03-02 16:38:17 -0500

    refactor customer handler

--- index.js
+++ index.js
@@ -139,7 +139,8 @@ function handle_customer(message) {
                 }
             );
         });
-    } else if (message.title == "What do you need help with?") {
+    }
+    if (message.title == "What do you need help with?") {
         DMS.sendTextMessage(
             customer.id, //
             customer.last_msg_id++, //Unique id of the message
