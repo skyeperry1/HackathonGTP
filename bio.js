9d48cd409b260707588c6abaa68d1b28f05329b2
Perry <perrs@l36774mus.lan>
2023-03-02 22:55:09 -0500

    add dynamic customer

--- bio.js
+++ bio.js
@@ -49,10 +49,10 @@ module.exports = class Bio {
             bio_prompt_txt += "\n";
         }
         bio_prompt_txt += "Hobbies:";
-        if (this.hobbies) {
-            this.hobbies.forEach(hobby => {
+        if (this.hobbies.length > 0) {
+            for (i = 0; i < this.hobbies.lenth; i++) {
                 bio_prompt_txt += hobby + ",";
-            });
+            }
         }
         bio_prompt_txt += "\n";
         bio_prompt_txt += "Patience Level(scale of 1-10):" + this.patience_level + "\n";
