13109fef1b745b99db0f1fe49450108416dfded8
Perry <perrs@l36774mus.lan>
2023-03-02 22:40:09 -0500

    add dynamic customer

--- bio.js
+++ bio.js
@@ -16,6 +16,7 @@ module.exports = class Bio {
         this.current_mood = bio.current_mood;
         this.personality_type = bio.personality_type;
         this.bio = bio.bio;
+        this.prompt_text = generateBioProptText();
     }
     address;
     name;
@@ -30,5 +31,32 @@ module.exports = class Bio {
     focus_level;
     bio;
     personality_type;
+    prompt_text;
+
+    static generateBioProptText() {
+        var bio_prompt_txt;
+        bio_prompt_txt += "Name: " + this.name + "\n";
+        bio_prompt_txt += "Address: " + this.address + "\n";
+
+        bio_prompt_txt += "Dob" + this.dob;
+        bio_prompt_txt += "Technological Skill(their ability to complete tasks related to tech, scale 1-10):" + this.technological_aptitude + "\n";
+        bio_prompt_txt += "I.Q.: " + this.iq + "\n";
+        bio_prompt_txt += "Occupation: " + this.occupation + "\n";
+        bio_prompt_txt += "Family:"
+        this.family.forEach(familyMemeber => {
+            bio_prompt_txt += "(" + familyMemeber.name + "," + familyMemeber, age, + "," + familyMemeber.relationship + ")";
+        });
+        bio_prompt_txt += "\nHobbies:";
+        this.hobbies.forEach(hobby => {
+            bio_prompt_txt += hobby + ",";
+        });
+        bio_prompt_txt += "\n";
+        bio_prompt_txt += "Patience Level(scale of 1-10):" + this.patience_level + "\n";
+        bio_prompt_txt += "Focus Level(scale of 1-10):" + this.focus_level + "\n";
+        bio_prompt_txt += "Current Mood:" + this.current_mood + "\n";
+        bio_prompt_txt += "Personality Type:" + this.personality_type + "\n";
+        bio_prompt_txt += "Bio: " + this.bio + "\n";
+        prompt_text = bio_prompt_txt;
+    }
 }
 
