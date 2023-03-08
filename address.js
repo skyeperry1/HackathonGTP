66ee72107beb277d995f3ea326eede26633d242d
Perry <perrs@l36774mus.rpega.com>
2023-03-08 09:44:03 -0500

    add create contact record, extemded customer profile

---
+++ address.js
@@ -0,0 +1,18 @@
+// address.js
+'use strict';
+
+module.exports = class Address {
+    constructor(street, city, state, zip) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+    street;
+    city;
+    state;
+    zip;
+
+
+}
+
